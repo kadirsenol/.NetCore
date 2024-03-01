92cedc94f67ff3f87a8957e501bbc1eba59ceb82
Kadir ŞENOL <kdrsnl_61@hotmail.com>
2024-03-01 23:05:06 +0300

    C#

--- ASP.NetCoreWebAppMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NetCoreWebAppMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
